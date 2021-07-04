a43dcddc97a5e6739a6f82ed008764413ba39bde
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-04 22:47:06 +0300

    Решение 3-й задачи корректировка

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -1,19 +1,20 @@
-def  sum_of_max(a_1, a_2, a_3):
-    if a_1 == a_2 or a_2 == a_3 or a_1 == a_3:
-        print('Введите различающиеся числа')
-        exit()
+def my_func(a_1, a_2, a_3):
     if a_1 < a_2:
         if a_1 < a_3:
-            sum = a_3 + a_2
+            return a_3 + a_2
         else:
-            sum =  a_1 + a_2
+            return a_1 + a_2
     if a_2 < a_1:
         if a_2 < a_3:
-            sum = a_1 + a_3
-    print('сумма двух наибольших чисел из введенных', sum)
+            return a_1 + a_3
 
-print(('Введите три разных числа'))
-a_1 = float(input('Введите 1-е число'))
-a_2 = float(input('Введите 2-е число'))
-a_3 = float(input('Введите 3-е число'))
-sum_of_max(a_1, a_2, a_3)
+
+while True:
+    print(('Введите три разных числа'))
+    a_1 = float(input('Введите 1-е число'))
+    a_2 = float(input('Введите 2-е число'))
+    a_3 = float(input('Введите 3-е число'))
+    if a_1 == a_2 or a_2 == a_3 or a_1 == a_3:
+        print('Введите различающиеся числа')
+        continue
+    print('сумма двух наибольших чисел из введенных', my_func(a_1, a_2, a_3))
