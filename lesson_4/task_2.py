3bed459b64b295121eda1c5bade52149448666f6
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-07 23:06:30 +0300

    reformat

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -1,4 +1,4 @@
 source_list = [12, 58, 14, 11, 55, 69, 1, -3]
 print(source_list)
-new_list = [source_list[i] for i in range(1,len(source_list)) if source_list[i]>source_list[i-1]]
+new_list = [source_list[i] for i in range(1, len(source_list)) if source_list[i] > source_list[i - 1]]
 print(new_list)
