b23bb2256cd63bee53c9b4bf1154a02eecfddc36
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-08 02:00:46 +0300

    Попытка исправить в работе с ГИТ

--- lesson_4/task_1.py
+++
@@ -1,6 +0,0 @@
-from sys import argv
-
-name, vyr, st, pr = argv
-zp = float(vyr) * float(st) + float(pr)
-print(f'Выработка сотрудника {vyr}, ставка {st}, премия {pr}, зарплата {zp}')
-
--- lesson_4/task_6.py
+++
@@ -1,4 +0,0 @@
-from itertools import islice, count
-source_list = [1, 2, 3, 4, 5]
-print(*(i for i in islice(count(int(input('Введите начальное значение'))), 10)), sep='\n')
-print(*(i for i in source_list))
