e25c9c5c0745b84a0b1d1fe2f4fc4ea708fc25f1
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-14 00:20:52 +0300

    Решение 1 задачи

---
+++ lesson_6/task_1.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+
+class trafficlight:
+    __color = {1: ['red', 7],
+               2: ['yellow', 2],
+               3: ['green', 4]}
+
+    def running(self):
+        while True:
+            for key in self.__color:
+                print(self.__color[key][0])
+                sleep(self.__color[key][1])
+
+
+trafficlight_1 = trafficlight()
+trafficlight_1.running()
