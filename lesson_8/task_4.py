632677348ba1c04911d2efbc74ab657918c0dea8
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-26 11:53:14 +0300

    Решение 4-й задачи

---
+++ lesson_8/task_4.py
@@ -0,0 +1,14 @@
+class Storage:
+    def __init__(self, name):
+        self.name = name
+
+class Equipment:
+    def __init__(self, type, price):
+        self.type = type
+        self.price = price
+
+
+
+
+
+
