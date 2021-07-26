cb09a14dbe75a5e5d5c5f39a139e22fc6e1dee25
Sergey Marchenko <marchenkosergey@mail.ru>
2021-07-26 12:47:21 +0300

    Решение 4-й задачи коррекция - добавлены классы потомки для класса Оргтехника

--- lesson_8/task_4.py
+++ lesson_8/task_4.py
@@ -3,9 +3,37 @@ class Storage:
         self.name = name
 
 class Equipment:
-    def __init__(self, type, price):
-        self.type = type
+    def __init__(self, new):
+        self.new = True
+
+    def __str__(self):
+        return (f"Состояние - {'Новый' if self.new else 'Б/у'}")
+
+
+class Printer(Equipment):
+    def __init__(self, type, vendor, model, price):
+        super().__init__(self.new)
+        self.type = 'Принтер'
+        self.vendor = vendor
+        self.model = model
         self.price = price
+
+class Scaner(Equipment):
+    def __init__(self, type, vendor, model, price):
+        super().__init__(self.new)
+        self.type = 'Сканнер'
+        self.vendor = vendor
+        self.model = model
+        self.price = price
+
+class Xerox(Equipment):
+    def __init__(self, type, vendor, model, price):
+        super().__init__(self.new)
+        self.type = 'Ксерокс'
+        self.vendor = vendor
+        self.model = model
+        self.price = price
+
 
 
 
